f95d8aa10ea4b0f7098232e7ee4864197e3a1d5f
Leticia244157 <leticia.grando.senai404@gmail.com>
2024-10-04 11:06:32 -0300

    Add files via upload

---
+++ html_php/index.php
@@ -0,0 +1,21 @@
+<?php  
+    $nome = 'Leticia';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem Vindo
+        <?php
+        echo ?>
+        <?php
+        echo '<h1> Olá Meu Senai';
+         ?>
+
+    </h1>
+</body>
+</html>
